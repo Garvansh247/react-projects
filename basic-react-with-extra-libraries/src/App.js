444e0633900aebbb4a56832724c22648e35fbd32
Garvansh <garvansh247@gmail.com>
2026-01-26 23:43:21 +0530

    basic exploration of react2

---
+++ basic-react-with-extra-libraries/src/App.js
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <>
+      <h1>Hello, React with Extra Libraries!</h1>
+      <p>This is a basic React application setup.</p>
+    </>
+  );
+}
+
+export default App;
